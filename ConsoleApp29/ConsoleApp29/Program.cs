eca7c8bfd4e9d88edbcdef23d5107da62e170946
VenkaPetrova <74990908+VenkaPetrova@users.noreply.github.com>
2024-01-13 14:20:07 +0200

    Add files via upload

---
+++ ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи а,b");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Всяко х е решение на линейното уравнение");
+
+                }
+                else
+                {
+                    Console.WriteLine("Линейното уравнение няма решение ");
+                }
+            }
+            else
+            {
+                double x = -b / a;
+                Console.WriteLine("x= " + x);
+            }
+            Console.ReadKey();
+        }  
+    }
+}
